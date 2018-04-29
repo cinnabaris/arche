c46016c1fd68047595b162364f3cb3d8d8633fd2
Jeremy Zheng <jitang.zheng@gmail.com>
2018-04-28 18:52:01 -0700

    feta(dashboard): add quill editor

--- desktop/src/plugins/index.js
+++ desktop/src/plugins/index.js
@@ -2,7 +2,12 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Vuex from 'vuex'
 import ElementUI from 'element-ui'
+import VueQuillEditor from 'vue-quill-editor'
+
 import 'element-ui/lib/theme-chalk/index.css'
+import 'quill/dist/quill.core.css'
+import 'quill/dist/quill.snow.css'
+import 'quill/dist/quill.bubble.css'
 
 import Application from '@/layouts/application'
 import Dashboard from '@/layouts/dashboard'
@@ -12,6 +17,7 @@ import forum from './forum'
 Vue.use(Router)
 Vue.use(Vuex)
 Vue.use(ElementUI)
+Vue.use(VueQuillEditor)
 
 Vue.component('layout-application', Application)
 Vue.component('layout-dashboard', Dashboard)
