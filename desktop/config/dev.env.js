915c8672c69901c80901f584961e5a8325299187
Jeremy Zheng <jitang.zheng@gmail.com>
2018-04-30 13:33:39 -0700

    refactor(ds): prepare using ant-design-pro sources

--- desktop/config/dev.env.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-const merge = require('webpack-merge')
-const prodEnv = require('./prod.env')
-
-module.exports = merge(prodEnv, {
-  NODE_ENV: '"development"',
-  BACKEND: '"http://localhost:8080/graphql"'
-})
