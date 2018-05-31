14369e79867e926acd43616ca161ad785fc90a6a
Jeremy Zheng <jitang.zheng@gmail.com>
2018-05-30 22:59:17 -0700

    refactor(db): remove mysql support

--- db/migrations/mysql/2018-04-12-195954_create_settings/up.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE settings (
-  id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  `key` VARCHAR(255) NOT NULL,
-  value BLOB NOT NULL,
-  salt BLOB,
-  created_at DATETIME NOT NULL DEFAULT NOW(),
-  updated_at DATETIME NOT NULL
-);
-CREATE UNIQUE INDEX idx_settings_key ON settings (`key`);
