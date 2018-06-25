82fbcb8029950b81bc51355d1467391aa1b6858d
Jeremy Zheng <jitang.zheng@gmail.com>
2018-06-25 11:37:46 -0700

    :bug: move building doc before binary

--- upgrade.sh
+++ upgrade.sh
@@ -9,8 +9,8 @@ cargo install --force diesel_cli
 cargo clean
 cargo update
 cargo check
-cargo build
 cargo doc
+cargo build
 cargo build --release
 
 # npm install --save antd ant-design-pro emoji-mart js-cookie jwt-decode moment moment-timezone qrcode.react react-color react-copy-to-clipboard react-google-maps react-intl react-loadable react-markdown react-moment react-quill react-redux react-router react-router-dom react-syntax-highlighter redux react-router-redux@next history react-document-title react-helmet graphql-request
