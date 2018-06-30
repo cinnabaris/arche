95d89543a2b209eb341163cbbbc427aa331d605a
Jeremy Zheng <jitang.zheng@gmail.com>
2018-06-30 15:48:08 -0700

    :bug: using string param for listLocaleByLang

--- src/graphql/query.rs
+++ src/graphql/query.rs
@@ -15,8 +15,8 @@ graphql_object!(Query: Context |&self| {
     field getSiteInfo(&executor) -> FieldResult<nut::graphql::models::SiteInfo> {
         ge!(nut::graphql::query::get_site_info(&executor.context()))
     }
-    field listLocalesByLang(&executor, form: nut::graphql::query::ListLocaleByLang) -> FieldResult<Vec<nut::graphql::models::Locale>> {
-        gq!(executor, form)
+    field listLocalesByLang(&executor, lang: String) -> FieldResult<Vec<nut::graphql::models::Locale>> {
+        gq!(executor, nut::graphql::query::ListLocaleByLang{lang: lang})
     }
 
     //--------------------forum--------------------
