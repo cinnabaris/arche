77e50440bb38be927d5bc00ea096b017de59fb41
Jeremy Zheng <jitang.zheng@gmail.com>
2018-07-04 11:01:01 -0700

    :heavy_plus_sign: add go packages

--- upgrade.sh
+++ upgrade.sh
@@ -16,6 +16,7 @@ govendor fetch github.com/BurntSushi/toml
 govendor fetch github.com/go-ini/ini
 govendor fetch golang.org/x/crypto/nacl/secretbox
 govendor fetch golang.org/x/crypto/nacl/auth
+govendor fetch golang.org/x/crypto/bcrypt
 govendor fetch golang.org/x/oauth2
 govendor fetch golang.org/x/oauth2/jwt
 govendor fetch golang.org/x/oauth2/google
@@ -25,5 +26,16 @@ govendor fetch github.com/facebookgo/inject
 govendor fetch github.com/jordan-wright/email
 govendor fetch github.com/sirupsen/logrus
 govendor fetch github.com/sirupsen/logrus/hooks/syslog
+govendor fetch github.com/spf13/viper
+govendor fetch github.com/google/uuid
+govendor fetch github.com/streadway/amqp
+govendor fetch github.com/gorilla/feeds
+govendor fetch github.com/ikeikeikeike/go-sitemap-generator/stm
+govendor fetch github.com/aws/aws-sdk-go/aws
+govendor fetch github.com/aws/aws-sdk-go/aws/awsutil
+govendor fetch github.com/aws/aws-sdk-go/aws/credentials
+govendor fetch github.com/aws/aws-sdk-go/aws/session
+govendor fetch github.com/aws/aws-sdk-go/service/s3
+
 
 # npm install --save antd ant-design-pro emoji-mart js-cookie jwt-decode moment moment-timezone qrcode.react react-color react-copy-to-clipboard react-google-maps react-intl react-loadable react-markdown react-moment react-quill react-redux react-router react-router-dom react-syntax-highlighter redux react-router-redux@next history react-document-title react-helmet graphql-request
