a31696fa98e521f14b5718627d928e83b8a19a16
Jeremy Zheng <jitang.zheng@gmail.com>
2018-07-05 13:18:33 -0700

    :building_construction: i18n::t macro

--- src/macros.rs
+++ src/macros.rs
@@ -4,6 +4,16 @@ macro_rules! l {
         l!($d, $u, $i, $l, $c, &None::<String>)
     };
     ($d:expr, $u:expr, $i:expr, $l:expr, $c:expr, $a:expr) => {
-        ::plugins::nut::dao::add_log($d, $u, $i, &::i18n::t($d, $l, &$c.to_string(), $a))
+        ::plugins::nut::dao::add_log($d, $u, $i, &t!($d, $l, &$c.to_string(), $a))
+    };
+}
+
+#[macro_export]
+macro_rules! t {
+    ($d:expr, $l:expr, $c:expr) => {
+        t!($d, $l, $c, &None::<String>)
+    };
+    ($d:expr, $l:expr, $c:expr, $a:expr) => {
+        ::i18n::t($d, $l, &$c.to_string(), $a)
     };
 }
