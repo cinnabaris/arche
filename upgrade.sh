ea78a9f2670480f5207157b8d24eb63f7e21d5bf
Jeremy Zheng <jitang.zheng@gmail.com>
2018-07-04 23:59:16 -0700

    :bug: using nightly

--- upgrade.sh
+++ upgrade.sh
@@ -2,16 +2,15 @@
 
 set -e
 
-rustup override set nightly
 rustup update
 cargo update
+cargo clean
 rustup component add rls-preview rust-analysis rust-src rustfmt-preview
 cargo install --force clippy
 cargo install --force diesel_cli
-cargo clean
 cargo check
-cargo doc
 cargo build
 cargo build --release
+cargo doc
 
 # npm install --save antd ant-design-pro emoji-mart js-cookie jwt-decode moment moment-timezone qrcode.react react-color react-copy-to-clipboard react-google-maps react-intl react-loadable react-markdown react-moment react-quill react-redux react-router react-router-dom react-syntax-highlighter redux react-router-redux@next history react-document-title react-helmet graphql-request
