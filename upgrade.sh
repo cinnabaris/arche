764f9643ea972f1eb0027a372434f3fb4d4b6e38
Jeremy Zheng <jitang.zheng@gmail.com>
2018-07-09 13:13:06 -0700

    :fire: remove document-title

--- upgrade.sh
+++ upgrade.sh
@@ -13,4 +13,4 @@ cargo build
 cargo build --release
 cargo doc
 
-# npm install --save antd ant-design-pro emoji-mart js-cookie jwt-decode moment moment-timezone qrcode.react react-color react-copy-to-clipboard react-google-maps react-intl react-loadable react-markdown react-moment react-quill react-redux react-router react-router-dom react-syntax-highlighter redux react-router-redux@next history react-document-title react-helmet graphql-request
+# npm install --save antd ant-design-pro emoji-mart js-cookie jwt-decode moment moment-timezone qrcode.react react-color react-copy-to-clipboard react-google-maps react-intl react-loadable react-markdown react-moment react-quill react-redux react-router react-router-dom react-syntax-highlighter redux react-router-redux@next history react-helmet graphql-request
