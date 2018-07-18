7736ca622381fc776f027f1142eaee3034f4a1f0
Jeremy Zheng <jitang.zheng@gmail.com>
2018-07-18 00:39:51 -0700

    :construction: col-form

---
+++ desktop/src/components/Form.vue
@@ -0,0 +1,11 @@
+<template>
+<el-col :md="{span:12}" :sm="{span: 24}">
+  <slot/>
+</el-col>
+</template>
+
+<script>
+export default {
+  name: 'Form'
+}
+</script>
--- desktop/src/components/index.js
+++ desktop/src/components/index.js
@@ -5,9 +5,11 @@ import IconFont from './IconFont'
 import Exception from './Exception'
 import TimeAgo from './TimeAgo'
 import Timestamp from './Timestamp'
+import Form from './Form'
 
 Vue.component('icon-font', IconFont)
 Vue.component('document-title', DocumentTitle)
 Vue.component('exception', Exception)
 Vue.component('time-ago', TimeAgo)
 Vue.component('timestamp', Timestamp)
+Vue.component('col-form', Form)
