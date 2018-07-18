c97cb2385d4e91b449b6206739bece716dcc78f6
Jeremy Zheng <jitang.zheng@gmail.com>
2018-07-18 11:46:19 -0700

    :bug: fix get_role bug

--- src/plugins/nut/dao/policy.rs
+++ src/plugins/nut/dao/policy.rs
@@ -25,15 +25,13 @@ pub fn groups(db: &Db, user: &i64) -> Result<Vec<String>> {
         .load::<(i64, NaiveDate, NaiveDate)>(db)?
     {
         if today.ge(&nbf) && today.le(&exp) {
-            let (name, rty, rid) = roles::dsl::roles
-                .select((
-                    roles::dsl::name,
-                    roles::dsl::resource_type,
-                    roles::dsl::resource_id,
-                ))
+            if let Ok(name) = roles::dsl::roles
+                .select(roles::dsl::name)
                 .filter(roles::dsl::id.eq(&rid))
-                .first::<(String, Option<String>, Option<i64>)>(db)?;
-            if None == rty && None == rid {
+                .filter(roles::dsl::resource_type.is_null())
+                .filter(roles::dsl::resource_id.is_null())
+                .first::<(String)>(db)
+            {
                 items.push(name);
             }
         }
@@ -54,13 +52,7 @@ pub fn can(
     resource_id: &Option<i64>,
 ) -> bool {
     let role = format!("{}", role);
-    if let Ok(role) = roles::dsl::roles
-        .select(roles::dsl::id)
-        .filter(roles::dsl::name.eq(&role))
-        .filter(roles::dsl::resource_type.eq(resource_type))
-        .filter(roles::dsl::resource_id.eq(resource_id))
-        .first::<i64>(db)
-    {
+    if let Ok(role) = get_role(db, &role, resource_type, resource_id) {
         if let Ok((nbf, exp)) = policies::dsl::policies
             .select((policies::dsl::nbf, policies::dsl::exp))
             .filter(policies::dsl::user_id.eq(user))
@@ -82,13 +74,7 @@ pub fn deny(
     resource_id: &Option<i64>,
 ) -> Result<()> {
     let role = format!("{}", role);
-    if let Ok(role) = roles::dsl::roles
-        .select(roles::dsl::id)
-        .filter(roles::dsl::name.eq(&role))
-        .filter(roles::dsl::resource_type.eq(resource_type))
-        .filter(roles::dsl::resource_id.eq(resource_id))
-        .first::<i64>(db)
-    {
+    if let Ok(role) = get_role(db, &role, resource_type, resource_id) {
         let it = policies::dsl::policies
             .filter(policies::dsl::user_id.eq(user))
             .filter(policies::dsl::role_id.eq(&role));
@@ -110,13 +96,7 @@ pub fn apply(
     let nbf = now.date();
     let exp = now.add(ttl).date();
 
-    let role = match roles::dsl::roles
-        .select(roles::dsl::id)
-        .filter(roles::dsl::name.eq(&role))
-        .filter(roles::dsl::resource_type.eq(resource_type))
-        .filter(roles::dsl::resource_id.eq(resource_id))
-        .first::<i64>(db)
-    {
+    let role = match get_role(db, &role, resource_type, resource_id) {
         Ok(id) => id,
         Err(_) => {
             let id = insert_into(roles::dsl::roles)
@@ -164,4 +144,34 @@ pub fn apply(
             Ok(id)
         }
     }
+}
+
+fn get_role(
+    db: &Db,
+    name: &String,
+    resource_type: &Option<String>,
+    resource_id: &Option<i64>,
+) -> Result<i64> {
+    if &None == resource_type {
+        return Ok(roles::dsl::roles
+            .select(roles::dsl::id)
+            .filter(roles::dsl::name.eq(name))
+            .filter(roles::dsl::resource_type.is_null())
+            .filter(roles::dsl::resource_id.is_null())
+            .first::<i64>(db)?);
+    }
+    if &None == resource_id {
+        return Ok(roles::dsl::roles
+            .select(roles::dsl::id)
+            .filter(roles::dsl::name.eq(name))
+            .filter(roles::dsl::resource_type.eq(resource_type))
+            .filter(roles::dsl::resource_id.is_null())
+            .first::<i64>(db)?);
+    }
+    Ok(roles::dsl::roles
+        .select(roles::dsl::id)
+        .filter(roles::dsl::name.eq(name))
+        .filter(roles::dsl::resource_type.eq(resource_type))
+        .filter(roles::dsl::resource_id.eq(resource_id))
+        .first::<i64>(db)?)
 }
