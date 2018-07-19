56799cf6e83aabeb8080cb188d116935fbf68d9e
Jeremy Zheng <jitang.zheng@gmail.com>
2018-07-18 22:12:12 -0700

    :bug: fix build

--- upgrade.sh
+++ upgrade.sh
@@ -11,7 +11,7 @@ rustup component add rls-preview rust-analysis rust-src rustfmt-preview
 cargo install --force diesel_cli
 cargo check
 cargo build
-cargo build --release
+make
 cargo doc
 
 # npm install --save antd ant-design-pro emoji-mart js-cookie jwt-decode moment moment-timezone qrcode.react react-color react-copy-to-clipboard react-google-maps react-intl react-loadable react-markdown react-moment react-quill react-redux react-router react-router-dom react-syntax-highlighter redux react-router-redux@next history react-helmet graphql-request
