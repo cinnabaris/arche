18cbcadbfe56e65efbc8e6cc5903eeb74bd8b2e6
Jeremy Zheng <jitang.zheng@gmail.com>
2018-07-28 08:11:22 -0700

    :arrow_up: upgrade packages

--- upgrade.sh
+++ upgrade.sh
@@ -3,15 +3,11 @@
 set -e
 
 rustup update
-# rustup override set nightly-2018-07-11
 cargo update
-rustup component add rls-preview rust-analysis rust-src rustfmt-preview
-# cargo install --force clippy
+rustup component add rls-preview rust-analysis rust-src rustfmt-preview clippy-preview
 cargo install --force diesel_cli
 make clean
 cargo check
 cargo build
 make
 cargo doc
-
-# npm install --save antd ant-design-pro emoji-mart js-cookie jwt-decode moment moment-timezone qrcode.react react-color react-copy-to-clipboard react-google-maps react-intl react-loadable react-markdown react-moment react-quill react-redux react-router react-router-dom react-syntax-highlighter redux react-router-redux@next history react-helmet graphql-request
